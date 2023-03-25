74f5d89435acb80197f007621cd60417c2e44efa
Kamal Maulaazka Sidhqi <90464322+kamalmlzka@users.noreply.github.com>
2023-03-25 11:59:20 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210032/Program.cs
+++ tpmodul6_1302210032/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Resources;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string judul)
+    {
+        Random idRandom = new Random();
+        this.id = idRandom.Next(0, 100000);
+
+        this.title = judul;
+        this.playCount = 0;
+    }
+
+    public void increasePlayCount(int playCount)
+    {
+        this.playCount = playCount;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine("Video " + title + " dengan ID " + id + " telah diputar sebanyak " + playCount + " Kali");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tutorial Design By Contract - KamalMaulaazkaSidhqi_Praktikan");
+        SayaTubeVideo vid = new SayaTubeVideo("Ramadhan");
+        vid.increasePlayCount(9);
+        vid.printVideoDetails();
+    }
+}
