6834c1f8914a8f76918648687f807bb0cc23f5fa
Kamal Maulaazka Sidhqi <90464322+kamalmlzka@users.noreply.github.com>
2023-03-26 15:07:34 +0700

    implementasi design by contract

--- tpmodul6_1302210032/Program.cs
+++ tpmodul6_1302210032/Program.cs
@@ -1,38 +1,52 @@
-﻿using System.Resources;
+﻿using System.Diagnostics;
 
-class SayaTubeVideo
+public class SayaTubeVideo
 {
     private int id;
     private string title;
     private int playCount;
 
-    public SayaTubeVideo(string judul)
+    public SayaTubeVideo(string title)
     {
-        Random idRandom = new Random();
-        this.id = idRandom.Next(0, 100000);
-
-        this.title = judul;
+        Debug.Assert(title.Length <= 100 && title != null, "Judul video memiliki panjang maksimal 100 karakter dan tidak berupa null.");
+        
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
         this.playCount = 0;
     }
 
-    public void increasePlayCount(int playCount)
+    public void IncreasePlayCount(int count)
     {
-        this.playCount = playCount;
+        Debug.Assert(count <= 10000000, "Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");
+
+        try
+        {
+            checked { this.playCount += count; }
+        }
+        catch (OverflowException err)
+        {
+            Console.WriteLine(err.Message);
+        }
     }
 
-    public void printVideoDetails()
+    public void PrintVideoDetails()
     {
-        Console.WriteLine("Video " + title + " dengan ID " + id + " telah diputar sebanyak " + playCount + " Kali");
+        Console.WriteLine("https://sayatube.com/watch?id=" + this.id);
+        Console.WriteLine();
+        Console.WriteLine(this.title);
+        Console.WriteLine(this.playCount.ToString("N0") + " views");
     }
 }
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Tutorial Design By Contract - KamalMaulaazkaSidhqi_Praktikan");
-        SayaTubeVideo vid = new SayaTubeVideo("Ramadhan");
-        vid.increasePlayCount(9);
-        vid.printVideoDetails();
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract – KamalMaulaazkaSidhqi_Praktikan");
+        for (int i = 0; i < 10000; i++)
+        {
+            vid.IncreasePlayCount(10000);
+        }
+        vid.PrintVideoDetails();
     }
 }
